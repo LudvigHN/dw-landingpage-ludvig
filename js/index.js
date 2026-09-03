c4b191670ebac7435f03af775377b7913f91e12f
LudvigHN <ludvighaukrognielsen@gmail.com>
2026-09-03 13:03:07 +0200

    script for printing hero

--- js/index.js
+++ js/index.js
@@ -7,16 +7,24 @@
 
 // hero
 
-
 const heroDOM = document.querySelector(".hero")
+
 const heroImg = document.createElement("img")
 const heroHeadline = document.createElement("h1")
 const heroP = document.createElement("p")
-const heroBtn = document.createElement ("button")
-heroImg.textContent = hero.image
+const heroBtn = document.createElement("button")
+const heroBtnImg = document.createElement("img")
+heroImg.setAttribute("src", hero.image)
 heroHeadline.textContent = hero.headline
 heroP.textContent = hero.copy
-heroBtn.textContent `${hero.icon} Explore`
+
+heroBtnImg.setAttribute("src", hero.icon)
+
+heroBtn.textContent = `Explore`
+heroBtn.prepend(heroBtnImg)
+const heroDiv = document.createElement("div")
+heroDiv.append(heroHeadline, heroP, heroBtn)
+heroDOM.append(heroImg, heroDiv)
 
 
 
