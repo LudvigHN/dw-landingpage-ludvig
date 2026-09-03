5a00780e6e0ae0d8764cc4a7bc85d060c70681fe
LudvigHN <ludvighaukrognielsen@gmail.com>
2026-09-03 12:43:47 +0200

    creating elements of hero with javaScript

--- js/index.js
+++ js/index.js
@@ -1,4 +1,18 @@
 // slå dig løs her... 
 
 //eksempel på at udskrive alle overskrifter i services i konsollen:
-services.forEach(service => console.log(service.headline))
+// services.forEach(service => console.log(service.headline))
+
+
+
+// hero
+
+
+const heroDOM = document.querySelector(".hero")
+const heroImg = document.createElement("img")
+const heroHeadline = document.createElement("h1")
+const heroP = document.createElement("p")
+const heroBtn = document.createElement ("button")
+
+
+
