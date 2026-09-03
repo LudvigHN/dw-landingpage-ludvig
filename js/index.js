1bbcbe1b22c31b34807203fdfeca39339b229871
LudvigHN <ludvighaukrognielsen@gmail.com>
2026-09-03 12:45:18 +0200

    hero textcontent

--- js/index.js
+++ js/index.js
@@ -13,6 +13,10 @@ const heroImg = document.createElement("img")
 const heroHeadline = document.createElement("h1")
 const heroP = document.createElement("p")
 const heroBtn = document.createElement ("button")
+heroImg.textContent = hero.image
+heroHeadline.textContent = hero.headline
+heroP.textContent = hero.copy
+heroBtn.textContent `${hero.icon} Explore`
 
 
 
